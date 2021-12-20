03fd4c4787f71e6cea8e13d24891c452d6ed9e60
AkKiL <78695802+HeimanPictures@users.noreply.github.com>
2021-12-20 17:45:25 +0530

    Typo 😜

--- database/verifier.py
+++ database/verifier.py
@@ -1,12 +1,12 @@
 
 import datetime
 
-import config
+from config import Config
 from handlers.database import Database
 
-DB_URL = config.DB_URL
-DB_NAME = config.DB_NAME
-LOG_CHANNEL = config.LOG_CHANNEL
+DB_URL = Config.DB_URL
+DB_NAME = Config.DB_NAME
+LOG_CHANNEL = Config.LOG_CHANNEL
 
 db = Database(DB_URL, DB_NAME)
 
